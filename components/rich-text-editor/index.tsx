dd7cf3a4eef9eea7531b31e0c5739db896d81f57
Puttipong <202909260+voaiutersna@users.noreply.github.com>
2025-12-10 17:56:30 +0700

    first-commit

---
+++ components/rich-text-editor/index.tsx
@@ -0,0 +1,45 @@
+"use client"
+import { useEditor, EditorContent } from "@tiptap/react"
+import StarterKit from "@tiptap/starter-kit" //รวม extensions พื้นฐาน
+import Menubar from "./menu-bar"
+import TextAlign from "@tiptap/extension-text-align"
+
+
+//EditorContent คือ React component ที่เอา editor มาวางบน DOM
+
+export default function RichtextEditor() {
+    const editor = useEditor({
+        extensions: [StarterKit.configure({
+            bulletList: {
+                HTMLAttributes: {
+                    class: 'list-disc ml-3',
+                }
+            },
+            orderedList: {
+                HTMLAttributes: {
+                    class: 'list-decimal ml-3',
+                },
+            }
+        }),
+        TextAlign.configure({
+            types: ["heading", "paragraph"]
+        })],
+        content: '<p>Hello World!</p>',
+        immediatelyRender: false, //กัน render โดยที่ไม่มีdom
+
+        //styling Editor
+        editorProps: {
+            attributes: {
+                class: "min-h-[156px] border rounded-md bg-slate-50 py-2 px-3"
+            }
+        }
+    })
+
+    return (
+        <div>
+            <Menubar editor={editor} />
+            <EditorContent editor={editor} />
+        </div>
+
+    )
+}
---
+++ components/rich-text-editor/menu-bar.tsx
@@ -0,0 +1,97 @@
+"use client"
+import {
+  AlignCenter,
+  AlignLeft,
+  AlignRight,
+  Bold,
+  Heading1,
+  Heading2,
+  Heading3,
+  Highlighter,
+  Italic,
+  List,
+  ListOrdered,
+  Strikethrough,
+} from "lucide-react";
+import { Toggle } from "../ui/toggle";
+import { Editor } from "@tiptap/react";
+
+//template code from this doc
+//https://tiptap.dev/docs/examples/basics/formatting
+
+
+export default function Menubar({ editor }:{ editor: Editor | null}) {
+    if (!editor) {
+        return null
+    }
+
+    const Options = [
+        {
+            icon: <Heading1 className="size-4" />,
+            onClick: () => editor.chain().focus().toggleHeading({ level: 1 }).run(),
+            preesed: editor.isActive("heading", { level: 1 }),
+        },
+        {
+            icon: <Heading2 className="size-4" />,
+            onClick: () => editor.chain().focus().toggleHeading({ level: 2 }).run(),
+            preesed: editor.isActive("heading", { level: 2 }),
+        },
+        {
+            icon: <Heading3 className="size-4" />,
+            onClick: () => editor.chain().focus().toggleHeading({ level: 3 }).run(),
+            preesed: editor.isActive("heading", { level: 3 }),
+        },
+        {
+            icon: <Bold className="size-4" />,
+            onClick: () => editor.chain().focus().toggleBold().run(),
+            preesed: editor.isActive("bold"),
+        },
+        {
+            icon: <Italic className="size-4" />,
+            onClick: () => editor.chain().focus().toggleItalic().run(),
+            preesed: editor.isActive("italic"),
+        },
+        {
+            icon: <Strikethrough className="size-4" />,
+            onClick: () => editor.chain().focus().toggleStrike().run(),
+            preesed: editor.isActive("strike"),
+        },
+        {
+            icon: <AlignLeft className="size-4" />,
+            onClick: () => editor.chain().focus().setTextAlign("left").run(),
+            preesed: editor.isActive({ textAlign: "left" }),
+        },
+        {
+            icon: <AlignCenter className="size-4" />,
+            onClick: () => editor.chain().focus().setTextAlign("center").run(),
+            preesed: editor.isActive({ textAlign: "center" }),
+        },
+        {
+            icon: <AlignRight className="size-4" />,
+            onClick: () => editor.chain().focus().setTextAlign("right").run(),
+            preesed: editor.isActive({ textAlign: "right" }),
+        },
+        {
+            icon: <List className="size-4" />,
+            onClick: () => editor.chain().focus().toggleBulletList().run(),
+            preesed: editor.isActive("bulletList"),
+        },
+        {
+            icon: <ListOrdered className="size-4" />,
+            onClick: () => editor.chain().focus().toggleOrderedList().run(),
+            preesed: editor.isActive("orderedList"),
+        },
+        {
+            icon: <Highlighter className="size-4" />,
+            onClick: () => editor.chain().focus().toggleHighlight().run(),
+            preesed: editor.isActive("highlight"),
+        },
+    ];
+    return (
+        <div className="border rounded-md p-1 mb-1 space-x-2 z-50 bg-slate-50">
+            {Options.map((item,index)=>(
+                <Toggle key={index} pressed={item.preesed} onPressedChange={item.onClick}>{item.icon}</Toggle>
+            ))}
+        </div>
+    )
+}
